8662750b55420111b82e2b67a27ab704b33624fe
UILabel <sp0302@126.com>
2016-03-23 11:23:16 +0800

    图片验证
    4位随机的数字或者字母的组合

--- ImageCertificationDemo/ImagCertificationView.h
+++ ImageCertificationDemo/ImagCertificationView.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 
 //图片验证
+//如果真的做的图片验证的话，可以写一个协议，然后在传出的协议中判断输验证图片里面的文字和键盘输入的文字是不是一致
 @interface ImagCertificationView : UIView
 
 @property (nonatomic, retain) NSArray *changeArray;
