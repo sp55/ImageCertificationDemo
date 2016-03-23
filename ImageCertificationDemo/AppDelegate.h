d8df76f8aa32e99b8c69c9fcc4886b7852a03e7a
sp55 <sp0302@126.com>
2016-03-23 10:59:26 +0800

    Initial Commit

---
+++ ImageCertificationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageCertificationDemo
+//
+//  Created by LuoYiJia on 16/3/23.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageCertificationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageCertificationDemo
+//
+//  Created by LuoYiJia on 16/3/23.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
